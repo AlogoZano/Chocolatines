ce4843b66493c08bd6e0e61ea9672af4af71bec2
AlogoZano <alg.lozgon@gmail.com>
2026-03-09 20:02:04 -0600

    Add extended views

--- chocolatines/base/admin.py
+++ chocolatines/base/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Spot, Chocolatin, ChocolatinScore
 
 # Register your models here.
+admin.site.register(Spot)
+admin.site.register(Chocolatin)
+admin.site.register(ChocolatinScore)
--- chocolatines/base/views.py
+++ chocolatines/base/views.py
@@ -1,16 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse
+from .models import Chocolatin, Spot
 
 # Create your views here.
 
 def ranked_list(request):
-    return render(request, 'ranked_list.html')
+    chocolatines = Chocolatin.objects.filter(is_published=True).order_by('-score')
+    return render(request, 'ranked_list.html', {'chocolatines': chocolatines})
 
 def ranked_detail(request):
     return render(request, 'ranked_detail.html')
 
 def spots_list(request):
-    return render(request, 'spots_list.html')
+    spots = Spot.objects.all()
+    return render(request, 'spots_list.html', {'spots': spots})
 
 def spot_detail(request):
     return render(request, 'spot_detail.html')
