7014bcb470255812d82990179468aa95ed8166a2
AlogoZano <alg.lozgon@gmail.com>
2026-03-10 20:03:45 -0600

    Add about view

--- chocolatines/base/views.py
+++ chocolatines/base/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse
-from .models import Chocolatin, Spot
+from .models import Chocolatin, Spot, ChocolatinScore
 
 # Create your views here.
 
@@ -8,15 +8,18 @@ def ranked_list(request):
     chocolatines = Chocolatin.objects.filter(is_published=True).order_by('-score')
     return render(request, 'ranked_list.html', {'chocolatines': chocolatines})
 
-def ranked_detail(request):
-    return render(request, 'ranked_detail.html')
+def ranked_detail(request, pk):
+    chocolatin = Chocolatin.objects.get(pk=pk)
+    rank = ChocolatinScore.objects.get(chocolatin=chocolatin)
+    return render(request, 'ranked_detail.html', {'chocolatin': chocolatin, 'rank': rank})
 
 def spots_list(request):
     spots = Spot.objects.all()
     return render(request, 'spots_list.html', {'spots': spots})
 
-def spot_detail(request):
-    return render(request, 'spot_detail.html')
+def spot_detail(request, pk):
+    spot = Spot.objects.get(pk=pk)
+    return render(request, 'spot_detail.html', {'spot': spot})
 
 def about(request):
     return render(request, 'about.html')
